b22f504baab8b108e62b7efef260db35dcdd796c
Yegor <viryutinyegor@gmail.com>
2023-04-25 19:43:03 +0300

    first try

--- kinoteatr/movies/urls.py
+++ kinoteatr/movies/urls.py
@@ -1,10 +1,20 @@
 # Создание URL-маршрутов
 # Определим URL-маршруты для нашего приложения
 
+# from django.urls import path
+# from . import views
+#
+# urlpatterns = [
+#     path('', views.home, name='home'),
+#     path('<int:movie_id>/', views.movie_detail, name='movie_detail'),
+# ]
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('<int:movie_id>/', views.movie_detail, name='movie_detail'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
